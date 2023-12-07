23f0311c987cf44c69eb737610d735ce135d0975
Grzetan <grzetan@gmail.com>
2023-12-07 12:24:19 +0100

    Implemented parsing xml file

--- src/parseBinary.cpp
+++ src/parseBinary.cpp
@@ -18,20 +18,46 @@ std::vector<ParamData> getRequiredParamData(
     throw std::runtime_error("Cannot open camera XML file");
   }
 
-  pugi::xpath_node_set query_result = doc.select_nodes(
-      "/AGV_COMM/TCP_FRAME[ID='6000']/SIGNAL_GROUP[SIGNAL[Name='SOC - State Of "
-      "Charge']]");
+  std::string query = "/AGV_COMM/TCP_FRAME[ID='6000']/SIGNAL_GROUP/SIGNAL[";
+  for (int i = 0; i < requiredFields.size(); i++) {
+    query += "Name='" + requiredFields[i] + "'";
+    if (i != requiredFields.size() - 1) {
+      query += " or ";
+    }
+  }
+  query += "]";
 
-  std::cout << query_result.size() << std::endl;
+  pugi::xpath_node_set query_result = doc.select_nodes(query.c_str());
 
-  return {};
+  std::vector<ParamData> output;
+
+  for (const auto& signal : query_result) {
+    std::string type = signal.node().child_value("VAR_TYPE");
+    uint64_t bit_offset = std::stol(signal.node().child_value("BIT_OFFSET"));
+    uint64_t signal_byte_offset =
+        std::stol(signal.node().child_value("BYTE_OFFSET"));
+
+    uint64_t group_byte_offset =
+        std::stol(signal.node().parent().child_value("OFFSET"));
+
+    output.push_back(
+        {group_byte_offset + signal_byte_offset, bit_offset, type});
+  }
+
+  return output;
 }
 
 int main(int argc, char** argv) {
-  std::vector<std::string> requiredFields = {"SOC - State Of Charge"};
+  std::vector<std::string> requiredFields = {
+      "SOC - State Of Charge", "Left drive activate - manual permission"};
 
   const auto& param_data =
       getRequiredParamData("./data/Forbot_v1.xml", requiredFields);
+
+  for (const auto& data : param_data) {
+    std::cout << data.type << ", " << data.byte_offset << ", "
+              << data.bit_offset << std::endl;
+  }
 
   return 0;
 }
