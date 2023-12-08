cfad698ee21ed670756e2cd34f42b78bf3332de3
Grzetan <grzetan@gmail.com>
2023-12-08 09:45:49 +0100

    Started implementing parsing string file and outputting csv

--- src/parseBinary.cpp
+++ src/parseBinary.cpp
@@ -1,4 +1,7 @@
+#include <algorithm>
+#include <iomanip>
 #include <iostream>
+#include <sstream>
 #include <string>
 #include <vector>
 
@@ -9,6 +12,7 @@ struct ParamData {
   uint64_t byte_offset;
   uint64_t bit_offset;
   std::string type;
+  std::string name;
 };
 
 std::vector<ParamData> getRequiredParamData(
@@ -42,8 +46,8 @@ std::vector<ParamData> getRequiredParamData(
     uint64_t group_byte_offset =
         std::stol(signal.node().parent().child_value("OFFSET"));
 
-    output.push_back(
-        {group_byte_offset + signal_byte_offset, bit_offset, type});
+    output.push_back({group_byte_offset + signal_byte_offset, bit_offset, type,
+                      signal.node().child_value("Name")});
   }
 
   return output;
@@ -67,8 +71,59 @@ std::vector<std::string> parseBytes(const std::vector<u_char>& bytes,
   return output;
 }
 
+void writeCSVHeader(std::ofstream& output,
+                    const std::vector<ParamData>& required_signals) {
+  output << "ID;";
+  for (const auto& sig : required_signals) {
+    output << sig.name << ";";
+  }
+}
+
+std::vector<u_char> string2bytes(std::string& line) {
+  line.erase(std::remove(line.begin(), line.end(), '"'));
+
+  std::stringstream ss(line.substr(1));
+
+  // Create a vector to store the resulting bytes
+  std::vector<u_char> byteVector = {0xA1};
+
+  // Iterate through pairs of characters and convert them to bytes
+  while (ss.good()) {
+    u_char byteValue;
+    ss >> std::hex >> byteValue;
+
+    // Check if the conversion was successful
+    if (ss.fail()) {
+      break;
+    }
+
+    // Add the byte to the vector
+    byteVector.push_back(static_cast<u_char>(byteValue));
+  }
+
+  // Print the resulting vector
+  for (const auto& byte : byteVector) {
+    std::cout << std::hex << std::setw(2) << std::setfill('0')
+              << static_cast<int>(byte);
+  }
+  std::cout << std::endl;
+
+  return {};
+}
+
 void binary2CSV(const std::string& binary_path, const std::string& output_path,
-                const std::vector<ParamData>& required_signals) {}
+                const std::vector<ParamData>& required_signals) {
+  std::ifstream data(binary_path, std::ios::binary);
+  std::ofstream output(output_path);
+
+  writeCSVHeader(output, required_signals);
+
+  std::string line;
+
+  while (std::getline(data, line)) {
+    std::vector<u_char> bytes = string2bytes(line);
+  }
+}
 
 int main(int argc, char** argv) {
   std::vector<std::string> required_fields = {"Current segment"};
