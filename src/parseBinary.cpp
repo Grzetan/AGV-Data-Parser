d23fb7d483f1a286a4bfd1ee2efdd480adc35835
Grzetan <grzetan@gmail.com>
2024-04-29 07:59:09 +0200

    Added fields

--- src/parseBinary.cpp
+++ src/parseBinary.cpp
@@ -6,7 +6,26 @@ int main(int argc, char** argv) {
   std::vector<std::string> required_fields = {
       "Time stamp", "Cumulative distance left", "Cumulative distance right",
       "ActualSpeed_L",
-      "ActualSpeed_R"};
+      "ActualSpeed_R",
+      "AGV velocity active zone",
+      "Front left strain gauge weight",
+      "Front right strain gauge weight",
+      "Rear left strain gauge weight",
+      "Rear right strain gauge weight",
+      "Momentary frequency of left encoder pulses",
+      "Momentary frequency of right encoder pulses",
+      "Cumulative distance left",
+      "Cumulative distance right",
+      "Momentary current consuption",
+      "Battery cell voltage",
+      "Momentary power consumption",
+      "Momentary energy consumption",
+      "Cumulative energy consumption",
+      "CR Momentary current consuption",
+      "CR Battery cell voltage",
+      "CR Momentary power consumption",
+      "CR Momentary energy consumption",
+      "CR Cumulative energy consumption"};
 
   const auto& required_signals =
       getRequiredParamData("./data/CoBotAGV_v2.xml", required_fields);
